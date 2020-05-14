fc63129f7f21399f00ead75dd71bc4c73a16bb37
root <root@84573d8fa669>
2020-05-14 01:28:41 +0000

    gnf

---
+++ src/_01_algorithms/_2_fibonacci/Fibonacci.java
@@ -0,0 +1,31 @@
+package _01_algorithms._2_fibonacci;
+
+public class Fibonacci {
+	public static void main(String[] args) {
+	int number1=0;
+    int number2=1;
+    int sum=1;
+    
+    System.out.println(number1);
+	System.out.println(number2);
+		
+	for (int i = 0; i < 30; i++) {
+		sum=number1+number2;
+	System.out.println(sum);
+	if(number1<number2) {
+		number1=sum;
+	}else {
+		number2=sum;
+	}
+	}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
